e0c084ae0472d77a21453d167358392c6091c0ed
Ivan Rakovic <ivan.rakovic@mogul.com>
2017-10-28 13:21:37 +0200

    Diameter and Manufacturer added

---
+++ Opticar/Models/Optics/Diameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Opticar.Models.Optics
+{
+    public class Diameter
+    {
+        public int DiameterId { get; set; }
+        public decimal Value { get; set; }
+    }
+}
---
+++ Opticar/Models/Optics/Manufacturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Opticar.Models.Optics
+{
+    public class Manufacturer
+    {
+        public int ManufacturerId { get; set; }
+        public string Name { get; set; }
+    }
+}
