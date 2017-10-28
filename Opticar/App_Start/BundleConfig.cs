d9a1cadea6f56cf00a12bbf543df17449c7de1f0
Ivan Rakovic <ivan.rakovic@mogul.com>
2017-10-28 12:00:59 +0200

    initial commit

---
+++ Opticar/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Opticar
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/ClientUI/js/jquery-3.2.1.min"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/ClientUI/js/popper.min.js",
+                      "~/ClientUI/js/mdb.js",
+                "~/ClientUI/js/bootstrap.js" ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/ClientUI/css/bootstrap.css",
+                      "~/ClientUI/css/style.css",
+                      "~/ClientUI/css/mdb.css"));
+        }
+    }
+}
---
+++ Opticar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Opticar.MvcApplication" Language="C#" %>
