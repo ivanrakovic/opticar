3aac74182749be192d799d30eee8267f9856780d
Ivan Rakovic <ivan.rakovic@mogul.com>
2017-10-28 13:35:30 +0200

    required attribute added

--- Opticar/Models/Optics/Diameter.cs
+++ Opticar/Models/Optics/Diameter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace Opticar.Models.Optics
     public class Diameter
     {
         public int DiameterId { get; set; }
+        [Required]
         public decimal Value { get; set; }
     }
 }
