675c3bea4680a1e930cffbb6d2367b8dc791e7d1
Ivan Rakovic <ivan.rakovic@mogul.com>
2017-10-28 13:35:16 +0200

    OpticsDbContext added

---
+++ Opticar/Data/OpticsDbContext .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Opticar.Data
+{
+    public class OpticsDbContext : DbContext
+    {
+        public OpticsDbContext()
+            : base("OpticsDb")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
