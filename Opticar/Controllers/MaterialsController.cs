6ddc988c94377eee7694132dd9c7d691c2259f2b
Ivan Rakovic <ivan.rakovic@mogul.com>
2017-10-29 17:17:34 +0100

    Materials controller and views added

---
+++ Opticar/Controllers/MaterialsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Opticar.Data;
+using Opticar.Models.Optics;
+
+namespace Opticar.Controllers
+{
+    public class MaterialsController : Controller
+    {
+        private readonly OpticsDbContext _db = new OpticsDbContext();
+
+        // GET: Materials
+        public ActionResult Index()
+        {
+            return View(_db.Materials.ToList());
+        }
+
+        // GET: Materials/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var material = _db.Materials.Find(id);
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
+        // GET: Materials/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Materials/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaterialId,Value")] Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Materials.Add(material);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(material);
+        }
+
+        // GET: Materials/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var material = _db.Materials.Find(id);
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Materials/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaterialId,Value")] Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(material).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(material);
+        }
+
+        // GET: Materials/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var material = _db.Materials.Find(id);
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Materials/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var material = _db.Materials.Find(id);
+            _db.Materials.Remove(material);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
